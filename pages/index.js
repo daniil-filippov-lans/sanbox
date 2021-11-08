d119b37d771fee36ac3f41e32b4370b95968b213
danil_lans <danimen0001@gmail.com>
2021-11-08 22:54:52 +0300

    format commit

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ export default function Home() {
     const { user, error, isLoading } = useUser();
 
     if (isLoading) return <p>...Loading</p>;
-    if (error) return <p>some errorss</p>;
+    if (error) return <p>some errors</p>;
 
     if (user) {
         return (
             <>
                 <h1>Welcome {user.name}</h1>
+                {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
                 <a href="/api/auth/logout">Logout</a>
             </>
         );
     }
 
+    /* eslint-disable-next-line @next/next/no-html-link-for-pages */
     return <a href="/api/auth/login">Login</a>;
 }
